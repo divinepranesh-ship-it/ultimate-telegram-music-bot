e247a92eb24d48ea11507d114f80bde895e5de97
divinepranesh-ship-it <divinepranesh@gmail.com>
2026-03-05 14:31:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7942415384:AAHDQO-7eMgMfHSRrSisNrSZ-KP_gJlA-VQ")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+OWNER_ID = int(os.getenv("OWNER_ID", "7853326776"))
